bc05e179852a1ef72557889daa5405d9a911e18b
TreanT <hntreant23@gmail.com>
2025-01-01 19:04:04 +0700

    update

--- contracts/MedicalApp.sol
+++ contracts/MedicalApp.sol
@@ -2,17 +2,9 @@
 
 pragma solidity >=0.4.22 <0.9.0;
 
-contract meDossier{
-    
-    uint256 public pindex=0;
-    uint256 public dindex=0;
-    
-    struct Records {
-    string dname;
-    string reason;
-    string visitedDate;
-    string ipfs;
-    }
+import "./patient.sol";
+
+contract meDossier is PatientContract{
 
     struct doctor {
         uint256 id;
@@ -25,28 +17,11 @@ contract meDossier{
         bool isApproved;
     }
     
-    struct patient{
-        uint256 id;
-        string name;
-        string phone;
-        string gender;
-        string dob;
-        string bloodgroup;
-        Records[] records;
-        address addr;
-    }
-    address[] private patientList;
     address[] public doctorList;
     uint256 [] public registeredDoctorList;
     address public  owner;
 
-    mapping(address=>patient) patients;
-    mapping(address=>doctor) doctors;
-    mapping(address=>bool) isPatient;
-    mapping(address=>bool) isDoctor;
-    mapping(address=>mapping(address=>bool)) Authorized;
-    mapping(string=>mapping(uint256=>bool)) Registered;
-    
+    mapping(address=>doctor) doctors;    
     
     constructor() {
         owner = msg.sender;
@@ -68,11 +43,6 @@ contract meDossier{
             return 3;
         }
     }
-    
-    function getPatientList() public view returns (address[] memory) {
-    return patientList;
-    }
-
 
     //Register the doctor by certain authority
     function registerDoctor(string memory docname, uint256 license) public {
@@ -109,111 +79,27 @@ contract meDossier{
         // patients[addr].records.push(Records(_dname,_reason,_visitedDate,_ipfs));
     }
     
-    //add the patient to the blockchain
-    function addPatient(string memory _name,string memory _phone,string memory _gender,string memory _dob,string memory _bloodgroup) public {
-        require(!isPatient[msg.sender],"Already Patient account exists");
-        require(bytes(_name).length>0);
-        require(bytes(_phone).length>0);
-        require(bytes(_gender).length>0);
-        require(bytes(_dob).length>0);
-        require(bytes(_bloodgroup).length>0);        
-        patientList.push(msg.sender);
-        pindex = pindex + 1;
-        isPatient[msg.sender]=true;
-        patients[msg.sender].id=pindex;
-        patients[msg.sender].name=_name;
-        patients[msg.sender].phone=_phone;
-        patients[msg.sender].gender=_gender;
-        patients[msg.sender].dob=_dob;
-        patients[msg.sender].bloodgroup=_bloodgroup;
-        patients[msg.sender].addr=msg.sender;
-        
-    }
-    
-    //get the details of the patients
-    function getPatientDetails(address _addr) public view returns(string memory _name,string memory _phone,string memory _gender,string memory _dob,string memory _bloodgroup){
-        require(isPatient[_addr],"No Patients found at the given address");
-        patient memory pat = patients[_addr];
-        return (pat.name,pat.phone,pat.gender,pat.dob,pat.bloodgroup);
-    }
 
-function getAllPatients() public view returns (
-    uint256[] memory ids,
-    address[] memory addresses,
-    string[] memory names,
-    string[] memory phones,
-    string[] memory genders,
-    string[] memory dobs,
-    string[] memory bloodgroups
-) {
-    uint256 totalPatients = patientList.length;
-    ids = new uint256[](totalPatients);
-    addresses = new address[](totalPatients);
-    names = new string[](totalPatients);
-    phones = new string[](totalPatients);
-    genders = new string[](totalPatients);
-    dobs = new string[](totalPatients);
-    bloodgroups = new string[](totalPatients);
-
-    for (uint256 i = 0; i < totalPatients; i++) {
-        address patientAddress = patientList[i];
-        patient memory pat = patients[patientAddress];
-
-        ids[i] = pat.id;
-        addresses[i] = patientAddress;
-        names[i] = pat.name;
-        phones[i] = pat.phone;
-        genders[i] = pat.gender;
-        dobs[i] = pat.dob;
-        bloodgroups[i] = pat.bloodgroup;
-    }
-}
   //get the length of records of particular address  
      function getrecordlist(address _addr)  public view returns (uint256 ){  
      return (patients[_addr].records.length);
      }
 
 //get the length of doctor's added in blockchain
-function getdoctorlist() public view returns(uint256){
-        return doctorList.length;
-    }
+// function getdoctorlist() public view returns(uint256){
+//         return doctorList.length;
+//     }
    
    //get the length of registered doctor 
-function getRegisteredDoctorslength() public view returns(uint256){
-        return registeredDoctorList.length;
-    }
+// function getRegisteredDoctorslength() public view returns(uint256){
+//         return registeredDoctorList.length;
+//     }
 
-//
-function getRegisteredDoctorsList(uint256 id) public view returns(uint256 license){
-        return registeredDoctorList[id];
-    }
+// //
+// function getRegisteredDoctorsList(uint256 id) public view returns(uint256 license){
+//         return registeredDoctorList[id];
+//     }
     
-//get patients record 
-function getAllPatientRecords(address _addr) 
-    public view 
-    returns(
-        string[] memory dnames, 
-        string[] memory reasons, 
-        string[] memory visitedDates, 
-        string[] memory ipfsHashes
-    ) 
-{
-    require(isPatient[_addr], "No patient found at the given address");
-    // require(Authorized[_addr][msg.sender] || msg.sender == _addr, "Record cannot be accessed");
-
-    uint256 recordCount = patients[_addr].records.length;
-    dnames = new string[](recordCount);
-    reasons = new string[](recordCount);
-    visitedDates = new string[](recordCount);
-    ipfsHashes = new string[](recordCount);
-
-    for (uint256 i = 0; i < recordCount; i++) {
-        dnames[i] = patients[_addr].records[i].dname;
-        reasons[i] = patients[_addr].records[i].reason;
-        visitedDates[i] = patients[_addr].records[i].visitedDate;
-        ipfsHashes[i] = patients[_addr].records[i].ipfs;
-    }
-}
 
 //add doctor 
     function addDoctor(string memory _name,string memory _hname,string memory _faculty,string memory _contact,uint256 license) public {
@@ -315,21 +201,21 @@ function getAllPatientRecords(address _addr)
     
 
 //Give access to certain address
-    function grantAccess(address _addr) public returns (bool success)
-    {   require(msg.sender != _addr,"You cannot add yourself");
-        require(isDoctor[_addr],"Not registered as doctor");
-        require(!Authorized[msg.sender][_addr],"User is already authorized");
-        Authorized[msg.sender][_addr] = true;
-        return true;
-    }
+//     function grantAccess(address _addr) public returns (bool success)
+//     {   require(msg.sender != _addr,"You cannot add yourself");
+//         require(isDoctor[_addr],"Not registered as doctor");
+//         require(!Authorized[msg.sender][_addr],"User is already authorized");
+//         Authorized[msg.sender][_addr] = true;
+//         return true;
+//     }
 
-//Revoke access of patient records from certain address
-      function revoke_access(address _addr)  public returns (bool success){
-        require(msg.sender!=_addr,"You can't remove yourself");
-        require(Authorized[msg.sender][_addr],"User is not authorized yet");
-        Authorized[msg.sender][_addr] = false;
-        return true;
-    }
+// //Revoke access of patient records from certain address
+//       function revoke_access(address _addr)  public returns (bool success){
+//         require(msg.sender!=_addr,"You can't remove yourself");
+//         require(Authorized[msg.sender][_addr],"User is not authorized yet");
+//         Authorized[msg.sender][_addr] = false;
+//         return true;
+    // }
 
 // function doctorLogin() public{
 //     if(Registered[doctors[msg.sender].name][doctors[msg.sender].licenseno] == true){
